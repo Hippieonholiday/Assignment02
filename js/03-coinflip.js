0cd4e6e75eac58e48a7976af7ed69e67ae3a8be9
Hippieonholiday <cassidybrynn@gmail.com>
2024-10-09 01:12:28 -0800

    update

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -1,19 +1,17 @@
-
-
-let coinFlip = Math.round(Math.random() * 100 + 1);
-let choice = prompt ("heads or tails");
+let coinFlip = Math.round(Math.random());
+let choice = prompt("heads or tails");
 
 switch (choice) {
     case "heads":
-        if (coinFlip < 50) {
+        if (coinFlip < 1) {
             alert ("The flip was heads and you chose heads...you win!");
-        } else if (coinFlip > 50) {
+        } else if (coinFlip > 0) {
             alert ("The flip was tails but you chose heads...you lose!");
         } break;
     case "tails":
-        if (coinFlip > 50) {
+        if (coinFlip > 0) {
             alert ("The flip was tails and you chose tails...you win!");
-        } else if (coinFlip < 50) {
+        } else if (coinFlip < 1) {
             alert ("The flip was heads but you chose tails...you lose!");
         } break;
     default:
